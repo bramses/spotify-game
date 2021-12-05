5fcced89eede587015ddcb2d5397dc7bc6a0613f
Ben Zenker <bzenker@themolecule.com>
2021-12-05 01:00:35 -0700

    added vote decisions to songpersonreveal page

--- pages/ui/songpersonreveal.js
+++ pages/ui/songpersonreveal.js
@@ -20,6 +20,24 @@ const SongPersonReveal = () => {
     )
   }, [])
 
+  const [votes, setVotes] = useState([
+    { id: 1, name: 'bram', votedFor: 3 },
+    { id: 2, name: 'melissa', votedFor: 2 },
+    { id: 3, name: 'ryan', votedFor: 2 },
+    { id: 4, name: 'glenn', votedFor: 2 },
+    { id: 5, name: 'caitlin', votedFor: 1 },
+    { id: 6, name: 'ben', votedFor: 5 },
+  ])
+  const votedFors = votes.reduce((accumulator, voter) => {
+    if (voter.votedFor in accumulator) {
+      accumulator[voter.votedFor].push(voter.name)
+    } else {
+      accumulator[voter.votedFor] = [voter.name]
+    }
+    return accumulator
+  }, {})
+  console.log(votedFors)
+
   return (
     <CenterContainer>
       {imageURL && (
@@ -37,6 +55,22 @@ const SongPersonReveal = () => {
       <h3 className={styles.namecontainer}>
         This song was added by <span className={styles.name}>{who}</span>!
       </h3>
+
+      {Object.entries(votedFors)
+        .sort((a, b) => (b[1].length - a[1].length))
+        .map(([selection, voters]) => {
+          const selectedPlayer = votes.find(p => `${p.id}` === selection)
+          const selectedPlayerName = selectedPlayer ? selectedPlayer.name : selection
+          return (
+            <div key={selection} className={styles.votercontainer}>
+              <div className={styles.voterselection}>
+                {voters.length} vote{voters.length !== 1 ? 's': ''} for <b>{selectedPlayerName}</b>
+              </div>
+              <div className={styles.voters}>{voters.join(', ')}</div>
+            </div>
+          )
+        }
+      )}
     </CenterContainer>
   );
 };
